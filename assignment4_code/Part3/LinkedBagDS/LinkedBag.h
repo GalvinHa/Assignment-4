fad2372d432e470864ff68078ee840e4182ff989
924468698 <924468698@sfsu.edu>
2025-11-22 14:20:03 -0800

    Update LinkedBag.h

--- assignment4_code/Part3/LinkedBagDS/LinkedBag.h
+++ assignment4_code/Part3/LinkedBagDS/LinkedBag.h
@@ -1,8 +1,8 @@
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
+//  Created by Frank M. Carrano and Timothy M. Henry.
+//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
 /** ADT bag: Link-based implementation.
-	 @file LinkedBag.h 
+	 @file LinkedBag.h
 	 Listing 4-3 */
 #ifndef LINKED_BAG_
 #define LINKED_BAG_
@@ -11,38 +11,44 @@
 #include "Node.h"
 
 template<class ItemType>
-class LinkedBag : public BagInterface<ItemType>{
-	private:
-		Node<ItemType>* headPtr; // Pointer to first node
-		int itemCount;			 // Current count of bag items
-		
-		// Returns either a pointer to the node containing a given entry
-		// or the null pointer if the entry is not in the bag.
-		Node<ItemType>* getPointerTo(const ItemType& target) const;
-		
-	public:
-		LinkedBag();
-		LinkedBag(const LinkedBag<ItemType>& aBag); // Copy constructor
-		virtual ~LinkedBag(); // Destructor should be virtual
-		
-		//   SORTING ----------------------------------------------------
-		void sort(int method=0);
-		// TO DO: update prototype of merge sort based on your implementation
-		void mergeSort(); 
-		// EXTRA CREDIT 
-		// TO DO: update prototype of quick sort based on your implementation
-		void quickSort();
-		// --------------------------------------------------------------
+class LinkedBag : public BagInterface<ItemType> {
+private:
+	Node<ItemType>* headPtr; // Pointer to first node
+	int itemCount;			 // Current count of bag items
 
-		int getCurrentSize() const;
-		bool isEmpty() const;
-		bool add(const ItemType& newEntry);
-		bool remove(const ItemType& anEntry);
-		void clear();
-		bool contains(const ItemType& anEntry) const;
-		int getFrequencyOf(const ItemType& anEntry) const;
-		std::vector<ItemType> toVector() const;
-		bool removeAlt(const ItemType& anEntry);
+	// Returns either a pointer to the node containing a given entry
+	// or the null pointer if the entry is not in the bag.
+	Node<ItemType>* getPointerTo(const ItemType& target) const;
+
+	//private helpers mergesort
+	Node<ItemType>* mergeSortedLists(Node<ItemType>* left, Node<ItemType>* right);
+	Node<ItemType>* mergeSortNodes(Node<ItemType>* head);
+	//private helpers quicksort
+	Node<ItemType>* quickSortRec(Node<ItemType>* head, Node<ItemType>* end);
+	Node<ItemType>* partition(Node<ItemType>* head, Node<ItemType>* end, Node<ItemType>** newHead, Node<ItemType>** newEnd);
+	Node<ItemType>* getTail(Node<ItemType>* current);
+
+public:
+	LinkedBag();
+	LinkedBag(const LinkedBag<ItemType>& aBag); // Copy constructor
+	virtual ~LinkedBag(); // Destructor should be virtual
+
+	//   SORTING ----------------------------------------------------
+	void sort(int method = 0);
+	void mergeSort();
+	// EXTRA CREDIT 
+	void quickSort();
+	// --------------------------------------------------------------
+
+	int getCurrentSize() const;
+	bool isEmpty() const;
+	bool add(const ItemType& newEntry);
+	bool remove(const ItemType& anEntry);
+	void clear();
+	bool contains(const ItemType& anEntry) const;
+	int getFrequencyOf(const ItemType& anEntry) const;
+	std::vector<ItemType> toVector() const;
+	bool removeAlt(const ItemType& anEntry);
 }; // end LinkedBag
 
 #include "LinkedBag.cpp"
