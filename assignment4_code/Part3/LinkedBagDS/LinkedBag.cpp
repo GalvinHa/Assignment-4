b9940f57e6c02593eebd2b1b8d182b6585395944
924468698 <924468698@sfsu.edu>
2025-11-22 14:20:32 -0800

    Refactor LinkedBag class and implement sorting methods

--- assignment4_code/Part3/LinkedBagDS/LinkedBag.cpp
+++ assignment4_code/Part3/LinkedBagDS/LinkedBag.cpp
@@ -1,5 +1,5 @@
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
+//  Created by Frank M. Carrano and Timothy M. Henry.
+//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
 /** ADT bag: Link-based implementation.
 	 @file LinkedBag.cpp */
@@ -11,109 +11,210 @@
 
 //   SORTING ----------------------------------------------------
 template<class ItemType>
-void LinkedBag<ItemType>::sort(int method){
+void LinkedBag<ItemType>::sort(int method) {
 	if (itemCount <= 1)
 		return;
-	
-	if (method == 0){
+
+	if (method == 0) {
 		// update function call if you change the prototype. 
 		mergeSort();
-	}else{
+	} 
+	else {
 		// for EXTRA CREDIT, update function call if you change the prototype.
 		// If you do NOT implement quickSort, NO action needed here
-		quickSort(); 
+		quickSort();
 	}
 }
-
-//TO DO: implement merge sort and change its prototype if you need to.
+//mergesort ----------------------------------------------------------------------------------------------------------
+// Merge two sorted linked lists
 template<class ItemType>
-void LinkedBag<ItemType>::mergeSort(){
+Node<ItemType>* LinkedBag<ItemType>::mergeSortedLists(Node<ItemType>* left, Node<ItemType>* right) {
+	if (!left) return right;
+	if (!right) return left;
 
-
+	Node<ItemType>* result = nullptr;
+	if (left->getItem() <= right->getItem()) {
+		result = left;
+		result->setNext(mergeSortedLists(left->getNext(), right));
+	}
+	else {
+		result = right;
+		result->setNext(mergeSortedLists(left, right->getNext()));
+	}
+	return result;
 }
 
+// Recursive merge sort on linked list nodes
+template<class ItemType>
+Node<ItemType>* LinkedBag<ItemType>::mergeSortNodes(Node<ItemType>* head) {
+	if (!head || !head->getNext()) return head;
+
+	// Split list in half
+	Node<ItemType>* slow = head;
+	Node<ItemType>* fast = head->getNext();
+	while (fast && fast->getNext()) {
+		slow = slow->getNext();
+		fast = fast->getNext()->getNext();
+	}
+
+	Node<ItemType>* mid = slow->getNext();
+	slow->setNext(nullptr);
+
+	// Recursive sort
+	Node<ItemType>* left = mergeSortNodes(head);
+	Node<ItemType>* right = mergeSortNodes(mid);
+
+	// Merge sorted halves
+	return mergeSortedLists(left, right);
+}
+
+// Merge sort public function
+template<class ItemType>
+void LinkedBag<ItemType>::mergeSort() {
+	headPtr = mergeSortNodes(headPtr);
+}
+//mergesort ----------------------------------------------------------------------------------------------------------
 //Extra Credit -- TO DO: implement quick sort and change its prototype 
 //                       if you need to.
+//quicksort ----------------------------------------------------------------------------------------------------------
 template<class ItemType>
-void LinkedBag<ItemType>::quickSort(){
-	
+void LinkedBag<ItemType>::quickSort() {
+	Node<ItemType>* tail = getTail(headPtr);
+	headPtr = quickSortRec(headPtr, tail);
+}
+
+// Recursive QuickSort
+template<class ItemType>
+Node<ItemType>* LinkedBag<ItemType>::quickSortRec(Node<ItemType>* head, Node<ItemType>* end) {
+	if (!head || head == end)
+		return head;
+
+	Node<ItemType>* newHead = nullptr;
+	Node<ItemType>* newEnd = nullptr;
+
+	Node<ItemType>* pivot = partition(head, end, &newHead, &newEnd);
+
+	// Sort the left part
+	if (newHead != pivot) {
+		Node<ItemType>* temp = newHead;
+		while (temp->getNext() != pivot)
+			temp = temp->getNext();
+		temp->setNext(nullptr);
+
+		newHead = quickSortRec(newHead, temp);
+
+		// Connect sorted left with pivot
+		Node<ItemType>* tailLeft = getTail(newHead);
+		tailLeft->setNext(pivot);
+	}
+
+	// Sort the right part
+	pivot->setNext(quickSortRec(pivot->getNext(), newEnd));
+
+	return newHead;
+}
+
+// Partition function
+template<class ItemType>
+Node<ItemType>* LinkedBag<ItemType>::partition(Node<ItemType>* head, Node<ItemType>* end,
+	Node<ItemType>** newHead, Node<ItemType>** newEnd) {
+	Node<ItemType>* pivot = end;
+	Node<ItemType>* prev = nullptr;
+	Node<ItemType>* curr = head;
+	Node<ItemType>* tail = pivot;
+
+	while (curr != pivot) {
+		if (curr->getItem() < pivot->getItem()) {
+			if (!*newHead)
+				*newHead = curr;
+			prev = curr;
+			curr = curr->getNext();
+		}
+		else {
+			if (prev)
+				prev->setNext(curr->getNext());
+			Node<ItemType>* temp = curr->getNext();
+			curr->setNext(nullptr);
+			tail->setNext(curr);
+			tail = curr;
+			curr = temp;
+		}
+	}
+
+	if (!*newHead)
+		*newHead = pivot;
+
+	*newEnd = tail;
+	return pivot;
+}
+
+// Get tail of list
+template<class ItemType>
+Node<ItemType>* LinkedBag<ItemType>::getTail(Node<ItemType>* current) {
+	while (current && current->getNext())
+		current = current->getNext();
+	return current;
 }
 // --------------------------------------------------------------
-
+//quicksort ----------------------------------------------------------------------------------------------------------
 template<class ItemType>
-LinkedBag<ItemType>::LinkedBag() : headPtr(nullptr), itemCount(0){
+LinkedBag<ItemType>::LinkedBag() : headPtr(nullptr), itemCount(0) {
 }  // end default constructor
 
 template<class ItemType>
-LinkedBag<ItemType>::LinkedBag(const LinkedBag<ItemType>& aBag){
+LinkedBag<ItemType>::LinkedBag(const LinkedBag<ItemType>& aBag) {
 	itemCount = aBag.itemCount;
 	Node<ItemType>* origChainPtr = aBag.headPtr;  // Points to nodes in original chain
-	
+
 	if (origChainPtr == nullptr)
 		headPtr = nullptr;  // Original bag is empty
 	else
 	{
-		// Copy first node
 		headPtr = new Node<ItemType>();
 		headPtr->setItem(origChainPtr->getItem());
-		
-		// Copy remaining nodes
+
 		Node<ItemType>* newChainPtr = headPtr;		// Points to last node in new chain
 		origChainPtr = origChainPtr->getNext();	  // Advance original-chain pointer
-		
+
 		while (origChainPtr != nullptr)
 		{
-			// Get next item from original chain
 			ItemType nextItem = origChainPtr->getItem();
-				  
-			// Create a new node containing the next item
 			Node<ItemType>* newNodePtr = new Node<ItemType>(nextItem);
-			
-			// Link new node to end of new chain
 			newChainPtr->setNext(newNodePtr);
-			
-			// Advance pointer to new last node
 			newChainPtr = newChainPtr->getNext();
-
-			// Advance original-chain pointer
 			origChainPtr = origChainPtr->getNext();
-		}  // end while
-		
-		newChainPtr->setNext(nullptr);				  // Flag end of chain
-	}  // end if
+		}
+		newChainPtr->setNext(nullptr);
+	}
 }  // end copy constructor
 
 template<class ItemType>
-LinkedBag<ItemType>::~LinkedBag(){
+LinkedBag<ItemType>::~LinkedBag() {
 	clear();
 }  // end destructor
 
 template<class ItemType>
-bool LinkedBag<ItemType>::isEmpty() const{
+bool LinkedBag<ItemType>::isEmpty() const {
 	return itemCount == 0;
-}  // end isEmpty
+}
 
 template<class ItemType>
-int LinkedBag<ItemType>::getCurrentSize() const{
+int LinkedBag<ItemType>::getCurrentSize() const {
 	return itemCount;
-}  // end getCurrentSize
+}
 
 template<class ItemType>
-bool LinkedBag<ItemType>::add(const ItemType& newEntry){
-	// Add to beginning of chain: new node references rest of chain;
-	// (headPtr is null if chain is empty)		  
+bool LinkedBag<ItemType>::add(const ItemType& newEntry) {
 	Node<ItemType>* nextNodePtr = new Node<ItemType>();
-
 	nextNodePtr->setItem(newEntry);
-	nextNodePtr->setNext(headPtr);  // New node points to chain
-	headPtr = nextNodePtr;			 // New node is now first node
+	nextNodePtr->setNext(headPtr);
+	headPtr = nextNodePtr;
 	itemCount++;
-	
 	return true;
-}  // end add
+}
 
 template<class ItemType>
-std::vector<ItemType> LinkedBag<ItemType>::toVector() const{
+std::vector<ItemType> LinkedBag<ItemType>::toVector() const {
 	std::vector<ItemType> bagContents;
 	Node<ItemType>* curPtr = headPtr;
 	int counter = 0;
@@ -122,121 +223,92 @@ std::vector<ItemType> LinkedBag<ItemType>::toVector() const{
 		bagContents.push_back(curPtr->getItem());
 		curPtr = curPtr->getNext();
 		counter++;
-	}  // end while
-	
+	}
 	return bagContents;
-}  // end toVector
+}
 
 template<class ItemType>
-bool LinkedBag<ItemType>::remove(const ItemType& anEntry){
+bool LinkedBag<ItemType>::remove(const ItemType& anEntry) {
 	Node<ItemType>* entryNodePtr = getPointerTo(anEntry);
 	bool canRemoveItem = !isEmpty() && (entryNodePtr != nullptr);
-	if (canRemoveItem){
-		// Copy data from first node to located node
+	if (canRemoveItem) {
 		entryNodePtr->setItem(headPtr->getItem());
-		
-		// Delete first node
 		Node<ItemType>* nodeToDeletePtr = headPtr;
 		headPtr = headPtr->getNext();
-		
-		// Return node to the system
 		nodeToDeletePtr->setNext(nullptr);
 		delete nodeToDeletePtr;
 		nodeToDeletePtr = nullptr;
-		
 		itemCount--;
-	} // end if
-	
+	}
 	return canRemoveItem;
-}  // end remove
+}
 
 template<class ItemType>
-bool LinkedBag<ItemType>::removeAlt(const ItemType& anEntry){
+bool LinkedBag<ItemType>::removeAlt(const ItemType& anEntry) {
 	Node<ItemType>* prev = headPtr;
 	Node<ItemType>* cur = headPtr;
 	bool found = false;
 
-	while((cur != nullptr) && !found){
+	while ((cur != nullptr) && !found) {
 		found = (*(cur->getItem()) == *anEntry);
-		if (!found){
+		if (!found) {
 			prev = cur;
 			cur = cur->getNext();
 		}
 	}
 
-	if (found){
+	if (found) {
 		prev->setNext(cur->getNext());
-		// delete cur->getItem();
 		delete cur;
 		cur = nullptr;
 		itemCount--;
 	}
 
 	return found;
-
-}  // end remove
+}
 
 template<class ItemType>
-void LinkedBag<ItemType>::clear(){
+void LinkedBag<ItemType>::clear() {
 	Node<ItemType>* nodeToDeletePtr = headPtr;
-	while (headPtr != nullptr){
+	while (headPtr != nullptr) {
 		headPtr = headPtr->getNext();
-
-		// Return node to the system
 		nodeToDeletePtr->setNext(nullptr);
-
-		// delete nodeToDeletePtr->getItem(); 
 		delete nodeToDeletePtr;
-		
 		nodeToDeletePtr = headPtr;
-	}  // end while
-	// headPtr is nullptr; nodeToDeletePtr is nullptr
-	
+	}
 	itemCount = 0;
-}  // end clear
+}
 
 template<class ItemType>
-int LinkedBag<ItemType>::getFrequencyOf(const ItemType& anEntry) const{
+int LinkedBag<ItemType>::getFrequencyOf(const ItemType& anEntry) const {
 	int frequency = 0;
 	int counter = 0;
 	Node<ItemType>* curPtr = headPtr;
 	while ((curPtr != nullptr) && (counter < itemCount))
 	{
-		if (anEntry == curPtr->getItem())
-		{
-			frequency++;
-		} // end if
-		
+		if (anEntry == curPtr->getItem()) frequency++;
 		counter++;
 		curPtr = curPtr->getNext();
-	} // end while
-	
+	}
 	return frequency;
-}  // end getFrequencyOf
+}
 
 template<class ItemType>
-bool LinkedBag<ItemType>::contains(const ItemType& anEntry) const{
+bool LinkedBag<ItemType>::contains(const ItemType& anEntry) const {
 	return (getPointerTo(anEntry) != nullptr);
-}  // end contains
+}
 
-
-// private
-// Returns either a pointer to the node containing a given entry 
-// or the null pointer if the entry is not in the bag.
 template<class ItemType>
-Node<ItemType>* LinkedBag<ItemType>::getPointerTo(const ItemType& anEntry) const{
+Node<ItemType>* LinkedBag<ItemType>::getPointerTo(const ItemType& anEntry) const {
 	bool found = false;
 	Node<ItemType>* curPtr = headPtr;
-	
+
 	while (!found && (curPtr != nullptr))
 	{
 		if (anEntry == curPtr->getItem())
 			found = true;
 		else
 			curPtr = curPtr->getNext();
-	} // end while
-	
+	}
 	return curPtr;
-} // end getPointerTo
-
-
+}
