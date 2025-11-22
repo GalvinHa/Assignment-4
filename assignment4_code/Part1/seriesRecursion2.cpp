10d3a3f01d2a19e5479f749a20f0f58e82f6c1aa
924468698 <924468698@sfsu.edu>
2025-11-22 14:16:36 -0800

    Implement recursive series function in C++

---
+++ assignment4_code/Part1/seriesRecursion2.cpp
@@ -0,0 +1,33 @@
+//TO DO: add your documentation here
+
+// TO DO: implement the recursive series function
+//      Hint: it should return a long long (integer)
+long long seriesRecursive(int n) {
+    // base case
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 2) {
+        return 1;
+    }
+    // recursive case
+    return seriesRecursive(n - 1) + 2 * seriesRecursive(n - 2) + 4 * seriesRecursive(n - 3);
+}
+
+
+int main(int argc, char* argv[]) {
+    // Read n from command line argument
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
+        return 1;
+    }
+    int n = std::atoi(argv[1]);
+
+    // Compute series value of n
+    long long result = seriesRecursive(n);
+    std::cout << "seriesRecursive(" << n << ") = " << result << std::endl;
+    return 0;
+}
